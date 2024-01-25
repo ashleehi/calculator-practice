75b5862c4b20c5d746ff79e1a6c2256f16181811
ashleehi <143832710+ashleehi@users.noreply.github.com>
2024-01-25 10:40:54 -0800

    JS

---
+++ calc.js
@@ -0,0 +1,17 @@
+display () function
+function display(val){
+    document.getElementById('result').value += val 
+    return val
+}
+
+solve () function
+function solve(){
+    let x=document.getElementById('result').value
+    let y=function(x)
+    document.getElementById('result').value=y
+    return y
+}
+
+function clearScreen(){
+    document.getElementById('result').value =''
+}
