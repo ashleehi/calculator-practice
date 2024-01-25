721fe67ddb12400c089cc8e6c31785889cf296af
ashleehi <ashlee.hi23@student.cart.org>
2024-01-25 15:08:39 -0800

    changed only everything

--- calc.js
+++ calc.js
@@ -1,17 +1,45 @@
-display () function
-function display(val){
-    document.getElementById('result').value += val 
-    return val
-}
+// display () function
+// function display(val){
+//     document.getElementById('result').value += val 
+//     return val
+// }
 
-solve () function
-function solve(){
-    let x=document.getElementById('result').value
-    let y=function(x)
-    document.getElementById('result').value=y
-    return y
-}
+// solve () function
+// function solve(){
+//     let x=document.getElementById('result').value
+//     let y=function(x)
+//     document.getElementById('result').value=y
+//     return y
+// }
 
-function clearScreen(){
-    document.getElementById('result').value =''
+// function clearScreen(){
+//     document.getElementById('result').value =''
+// }
+
+const input = document.getElementById('inputtext');
+const buttons = document.querySelectorAll('button');
+function calculate(expression) {
+    console.log(expression);
+    try {
+        return new Function('return ' + expression)();
+    } catch (error) {
+        return 'Malformed Operation';
+    }
 }
+function operation(buttonValue) {
+    if (buttonValue === 'C') {
+        input.value = '';
+    } else if (buttonValue === 'DEL') {
+        input.value = input.value.slice(0, -1);
+    } else if (buttonValue === '=') {
+        input.value = calculate(input.value);
+    } else {
+        input.value += buttonValue;
+    }
+}
+buttons.forEach(button => {
+    let buttonValue = button.innerText;
+    button.addEventListener('click', function () {
+        operation(buttonValue);
+    });
+});
